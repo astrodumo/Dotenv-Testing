5f7a3c6a347e40c83193588d0548c5aa091e1c2c
astrodumo <ibidumo3.com>
2024-04-12 22:09:36 +0100

    initial

--- envy.js
+++ envy.js
@@ -1,5 +1,6 @@
-function envy() {
-    alert("Get a Life!!")    
-};
+//Testing environment variables with dotenv module
 
+function envy() {
+    alert("It is in the character of very few men to honor without envy a friend who has prospered.")    
+};
 envy();
